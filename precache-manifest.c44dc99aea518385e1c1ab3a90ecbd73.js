1b9116a52313feb9db0cca866f4faca9e3488d8c
Boon <harrisonboon@gmail.com>
2020-08-10 22:49:01 +0100

    Updates

---
+++ precache-manifest.c44dc99aea518385e1c1ab3a90ecbd73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011867fbe61e208dc17f50267165487b",
+    "url": "/boon/index.html"
+  },
+  {
+    "revision": "17e53f4fc850c3a10c66",
+    "url": "/boon/static/css/main.d4e7e155.chunk.css"
+  },
+  {
+    "revision": "4e5c3dc67af27eb949fc",
+    "url": "/boon/static/js/2.4cb98a39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boon/static/js/2.4cb98a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e53f4fc850c3a10c66",
+    "url": "/boon/static/js/main.11090bd2.chunk.js"
+  },
+  {
+    "revision": "deb3c308020b41274db4",
+    "url": "/boon/static/js/runtime-main.b095ebfe.js"
+  },
+  {
+    "revision": "dba0c688b8d5ee09a1e214aebd5d25e4",
+    "url": "/boon/static/media/Product Sans Bold.dba0c688.ttf"
+  },
+  {
+    "revision": "eae9c18cee82a8a1a52e654911f8fe83",
+    "url": "/boon/static/media/Product Sans Regular.eae9c18c.ttf"
+  }
+]);
