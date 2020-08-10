41196f00747d9cd45793bb2563b0519dee17db30
Boon <harrisonboon@gmail.com>
2020-08-10 22:34:12 +0100

    a

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const barStyle = {
+  "flex-basis": "calc(100% - 80px)"
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Reveal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      val: this.props.val,
+      name: this.props.name,
+      value: this.props.name,
+    };
+  }
+
+  render() {
+    return (
+      <div
+        class="cell"
+        onMouseEnter={() => this.setState({value: this.state.val})}
+        onMouseLeave={() => this.setState({value: this.state.name})}
+      >
+        <a>{this.state.value}</a>
+      </div>
+    );
+  }
+}
+
+const App = () => {
+  return (
+  <div class="grid">
+    <div class="cell"><a href="https://github.com/boon-cpu">Github</a></div>
+    <div class="cell"><a href="https://twitter.com/B00N9999">Twitter</a></div>
+    <div class="cell"><a href="https://www.instagram.com/boon_cpuy/">Instergram</a></div>
+    <div class="bar unselectable" style={barStyle}><a>boon-cpu</a></div>
+    <Reveal val="Boon#0001" name="Discord"/>
+    <Reveal val="harrisonboon@gmail.com" name="Email"/>
+    <div class="cell"><a>--TBD--</a></div>
+  </div>
+  );
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
