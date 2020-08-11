c38e2e5b40e81699baaff1247932ed1dcca74443
Boon <harrisonboon@gmail.com>
2020-08-11 15:09:14 +0100

    just changed it m9

--- src/index.js
+++ src/index.js
@@ -1,45 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+const contents = ["React", "Node.js", "TypeScript"]
+let i=0;
 
-const barStyle = {
-  "flex-basis": "calc(100% - 80px)",
-  "font-size": "8vmin"
-}
-
-class Reveal extends React.Component {
-  constructor(props) {
-    super(props);
+class Cycle extends React.Component {
+  constructor(props){
+    super(props)
     this.state = {
-      val: this.props.val,
-      name: this.props.name,
-      value: this.props.name,
-    };
+      contents: this.props.contents,
+      value: contents[0],
+    }
   }
-
-  render() {
+  render(){
     return (
-      <div
-        class="cell"
-        onMouseEnter={() => this.setState({value: this.state.val})}
-        onMouseLeave={() => this.setState({value: this.state.name})}
-      >
-        <a>{this.state.value}</a>
-      </div>
-    );
+      <a onClick={() => {
+        i++
+        if(i>2)i=0
+        this.setState({value: this.state.contents[i]})
+      }}>{this.state.value}</a>
+    )
   }
 }
 
 const App = () => {
   return (
   <div class="grid">
-    <div class="cell"><a href="https://github.com/boon-cpu">Github</a></div>
-    <div class="cell"><a href="https://twitter.com/B00N9999">Twitter</a></div>
-    <div class="cell"><a href="https://www.instagram.com/boon_cpuy/">Instergram</a></div>
-    <div class="bar unselectable" style={barStyle}><a>boon-cpu</a></div>
-    <Reveal val="Boon#0001" name="Discord"/>
-    <Reveal val="harrisonboon@gmail.com" name="Email"/>
-    <div class="cell"><a>--TBD--</a></div>
+    <div class="cell hoverable unselectable"><a>Harrison Boon</a></div>
+    <div class="cell unselectable invisible"></div>
+    <div class="cell unselectable invisible"></div>
+    <div class="cell big hoverable unselectable"><a class="boon">Hi there, I'm Boon,<br/>and I am a full-stack <Cycle contents={contents}/> developer</a></div>
   </div>
   );
 }
