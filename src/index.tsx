4fd62ba651b35a2b0e9dc957c27109072d3b7408
Boon <harrisonboon@gmail.com>
2020-08-18 15:07:40 +0100

    moved to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-const boon = "https://cdn.discordapp.com/attachments/733615460108599326/744492234048798780/v_-_Copy.png"
-let i = 0;
+const boon: string = "https://cdn.discordapp.com/attachments/733615460108599326/744492234048798780/v_-_Copy.png"
+let i: number = 0;
 
 const Intro = () => {
   return(
     <h1 id="intro">Hello. <img onClick={() => {
       if (i % 2 === 0) {
-        document.getElementById('App').style.background = '#222831'
-        document.getElementById('intro').style.color = "#FBD46D"
-        document.getElementById('body').style.color = "#FBD46D"
-        document.getElementById('body2').style.color = "#FBD46D"
+        document.getElementById('App')!.style.background = '#222831'
+        document.getElementById('intro')!.style.color = "#FBD46D"
+        document.getElementById('body')!.style.color = "#FBD46D"
+        document.getElementById('body2')!.style.color = "#FBD46D"
         i+=1;
       } else {
-        document.getElementById('App').style.background = "#EEEEEE"
-        document.getElementById('intro').style.color = "#4F8A8B"
-        document.getElementById('body').style.color = "#4F8A8B"
-        document.getElementById('body2').style.color = "#4F8A8B"
+        document.getElementById('App')!.style.background = "#EEEEEE"
+        document.getElementById('intro')!.style.color = "#4F8A8B"
+        document.getElementById('body')!.style.color = "#4F8A8B"
+        document.getElementById('body2')!.style.color = "#4F8A8B"
         i+=1;
       }
     }}src={boon} alt=""/></h1>
