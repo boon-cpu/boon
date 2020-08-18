d254ca5856a306b020a2c04bc48d3f945df28015
Boon <harrisonboon@gmail.com>
2020-08-18 04:02:06 +0100

    made the webseite

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-const boon = "https://cdn.discordapp.com/attachments/434428050873122827/745092557041565898/v_-_Copy_1.png"
+const boon = "https://cdn.discordapp.com/attachments/733615460108599326/744492234048798780/v_-_Copy.png"
 let i = 0;
 
 const Intro = () => {
@@ -10,13 +10,26 @@ const Intro = () => {
       if (i % 2 === 0) {
         document.getElementById('App').style.background = '#222831'
         document.getElementById('intro').style.color = "#FBD46D"
+        document.getElementById('body').style.color = "#FBD46D"
+        document.getElementById('body2').style.color = "#FBD46D"
         i+=1;
       } else {
         document.getElementById('App').style.background = "#EEEEEE"
         document.getElementById('intro').style.color = "#4F8A8B"
+        document.getElementById('body').style.color = "#4F8A8B"
+        document.getElementById('body2').style.color = "#4F8A8B"
         i+=1;
       }
     }}src={boon} alt=""/></h1>
+  )
+}
+
+const Body = () => {
+  return(
+    <div>
+      <p id="body">I'm a 16 year old dev from the UK.<br/>I aim to innovate and improve every solution to any problem.</p>
+      <p id="body2">I specialise in Node, React and Typescript development,<br/>but still maintain knowledge in others.</p>
+    </div>
   )
 }
 
@@ -25,6 +38,9 @@ const App = () => {
     <div className="App" id="App">
       <div className="header">
         <Intro/>
+      </div>
+      <div className="body">
+        <Body />
       </div>
     </div>
   );
