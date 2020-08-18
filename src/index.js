7e0e051af97e13fd34fe85024d844174463f7e5c
Boon <harrisonboon@gmail.com>
2020-08-18 02:32:22 +0100

    fixed image size lighthouse warning

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-const boon = "https://cdn.discordapp.com/attachments/733615460108599326/744492234048798780/v_-_Copy.png?size=128"
+const boon = "https://cdn.discordapp.com/attachments/434428050873122827/745092557041565898/v_-_Copy_1.png"
 let i = 0;
 
 const Intro = () => {
